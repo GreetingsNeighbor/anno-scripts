b10a8f141e1174023c4f570bbc99aadf2d5e7363
GreetingsNeighbor <tu@tunguyen.com>
2022-04-18 21:57:08 -0400

    updated file name from pix to matchImage and main to mainscreen, optimized image loading

--- imaginereg.py
+++ imaginereg.py
@@ -8,21 +8,18 @@ from PIL import ImageGrab
 import pyautogui
 
 
-# Read the main image
-img_rgb = cv2.imread('pix.jpg')
+# # Read the main image
+# img_rgb = cv2.imread('matchImage.jpg')
 
-# # Convert it to grayscale
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
+# # # Convert it to grayscale
+# img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+# Read the template 
+template = cv2.imread('matchImage.jpg', 0)
+# Store width and height of template in w and h
+w, h = template.shape[::-1]
 while(True):
     screen = np.array(ImageGrab.grab())
     screen = cv2.cvtColor(src=screen, code=cv2.COLOR_BGR2GRAY)
-
-    # Read the template
-    template = cv2.imread('pix.jpg', 0)
-
-    # Store width and height of template in w and h
-    w, h = template.shape[::-1]
 
     # Perform match operations.
     res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
@@ -41,9 +38,6 @@ while(True):
         time.sleep(5)
         loc = []
 
-        # cv2.rectangle(screen, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
-        # cv2.imshow('my_screen', screen)
-        # cv2.waitKey(0)
 
     # cv2.imshow('my_screen', screen)
     # press escape to exit
